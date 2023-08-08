b7c89e8f14d81240adc7e8b93e842f694912f31a
Cristian <cristiankhan23@gmail.com>
2023-08-08 11:26:07 -0300

    Finished union operation

--- operations.hs
+++ operations.hs
@@ -1,21 +1,36 @@
--- Implementation of sets operations like union, intersection, complement, diference and simetric diference.
+-- Implementation of sets operations like UNION, INTERSECTION, COMPLEMENT, DIFERENCE and SIMETRIC DIFERENCE.
 
 -- Union of sets
-union :: Eq a => [a] -> [a] -> [a]
+union :: Ord a => [a] -> [a] -> [a]
 union [] [] = []
 union xs [] = xs
 union [] ys = ys
--- usar una funcion merge sort y eliminar repetidos.
+union (x:xs) (y:ys) = deleteRepElem (mergeSort ((x:xs) ++ (y:ys)))
 
--- mergeSort returns a ordered list of elements
+-- mergeSort returns a ordered list of elements.
 mergeSort :: Ord a => [a] -> [a]
 mergeSort [] = []
 mergeSort [x] = [x]
 mergeSort xs = merge (mergeSort (fstHalf xs)) (mergeSort (sndHalf xs))
 
--- merge is invocated in function mergeSort. Order two halves of one list.
+-- merge is invocated in mergeSort function. Order two halves of a list.
 merge :: Ord a => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
 merge (x:xs) (y:ys) | x <= y = x : merge xs (y:ys)
-                    | otherwise = y : merge (x:xs) ys 
+                    | otherwise = y : merge (x:xs) ys
+
+-- take n first elements of a list, is invocated in mergeSort function.
+fstHalf :: Ord a => [a] -> [a]
+fstHalf xs = take (div (length xs) 2) xs
+
+-- drop n first elements of a list, is invocated in mergeSort function.
+sndHalf :: Ord a => [a] -> [a]
+sndHalf xs = drop (div (length xs) 2) xs
+
+-- delete repeated elements of a list, this function require a ordered list.
+deleteRepElem :: Ord a => [a] -> [a]
+deleteRepElem [] = []
+deleteRepElem [x] = [x]
+deleteRepElem (x:y:ys) | x == y = deleteRepElem (y:ys)
+                       | otherwise = x : deleteRepElem (y:ys)
