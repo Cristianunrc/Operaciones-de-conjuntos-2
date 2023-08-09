4eb5628997b2c4da6418099e25bf45739840034e
Cristian <cristiankhan23@gmail.com>
2023-08-09 10:03:52 -0300

    Finished complement operation

--- Operations/operations.hs
+++ Operations/operations.hs
@@ -1,6 +1,7 @@
 -- Implementation of sets operations like UNION, INTERSECTION, COMPLEMENT, DIFERENCE and SIMETRIC DIFERENCE.
 
 -- ========== Union of sets ==========
+-- first list is A set and second list is B set.
 union :: Ord a => [a] -> [a] -> [a]
 union [] [] = []
 union xs [] = xs
@@ -36,10 +37,12 @@ deleteRepElem (x:y:ys) | x == y = deleteRepElem (y:ys)
                        | otherwise = x : deleteRepElem (y:ys)
 
 -- ========== Intersection of sets ==========
+-- first list is A set and second list is B set.
 intersection :: Ord a => [a] -> [a] -> [a]
 intersection xs ys = intersectionAux (mergeSort xs) (mergeSort ys)
 
--- intersection operation that work with two ordered list. 
+-- intersection operation is invocated in intersection function.
+-- return the intersection of two sets, which is A and B. 
 intersectionAux :: Ord a => [a] -> [a] -> [a]
 intersectionAux _ [] = []
 intersectionAux [] _ = []
@@ -48,10 +51,12 @@ intersectionAux (x:xs) (y:ys) | x == y = x : intersectionAux xs ys
                               | x > y = intersectionAux (x:xs) ys 
 
 -- ========== Diference of sets ==========
+-- first list is A set and second list is B set.
 diference :: Ord a => [a] -> [a] -> [a]
 diference xs ys = diferenceAux (mergeSort xs) (mergeSort ys)
 
--- diference operation that work with two ordered list.
+-- diference operation is invocated in diference function.
+-- return the diference of two sets, which is A and B.
 diferenceAux :: Ord a => [a] -> [a] -> [a]
 diferenceAux [] _ = []
 diferenceAux xs [] = xs
@@ -61,4 +66,17 @@ diferenceAux (x:xs) (y:ys) | include x (y:ys) = diferenceAux xs (y:ys)
 -- return a boolean that said if an element is included in a list.
 include :: Ord a => a -> [a] -> Bool
 include x [] = False
-include x (y:ys) = x == y || include x ys                            
+include x (y:ys) = x == y || include x ys
+
+-- ========== Complement of sets ==========
+-- first list is Universal set and second list is A set.
+complement :: Ord a => [a] -> [a] -> [a]
+complement us xs = complementAux (mergeSort us) (mergeSort xs)
+
+-- complement operation is invocated in complement function.
+-- return the complement of a set, considering the universal set.
+complementAux :: Ord a => [a] -> [a] -> [a]
+complementAux [] _ = []
+complementAux us [] = us
+complementAux (u:us) (x:xs) | include u (x:xs) = complementAux us (x:xs)
+                            | otherwise = u : complementAux us (x:xs)
